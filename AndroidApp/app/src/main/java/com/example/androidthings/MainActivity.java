90df115f73d68c3b8f593baef1b7dfd251304a3b
Filip Rosian <rosian.filip@gmail.com>
2020-06-12 01:50:54 +0300

    Working image post

--- AndroidApp/app/src/main/java/com/example/androidthings/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidthings/MainActivity.java
@@ -1,13 +1,98 @@
 package com.example.androidthings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void takeAndSendPicture() {
+
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+
+                System.out.println("Photo sent at " + LocalDateTime.ofInstant(Instant.ofEpochMilli(scheduledExecutionTime()), ZoneId.systemDefault()));
+            }
+        };
+
+        Timer timer = new Timer("Timer");
+        long delay = 10000L;
+        timer.schedule(task, 0, delay);
+    }
+
+
+
+
+
+    CameraControllerWithoutPreview cameraControllerWithoutPreview = new CameraControllerWithoutPreview(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraControllerWithoutPreview.openCamera();
+        try { Thread.sleep(20); } catch (InterruptedException e) {}
+        cameraControllerWithoutPreview.takePicture();
+
+
+        final TextView infoText = (TextView) findViewById(R.id.infoText);
+        infoText.setText("Maaaaaaaaaaaaaaaai");
+
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.100.25:3000/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        infoText.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                infoText.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- WebServer/app.js
+++ WebServer/app.js
@@ -1,14 +1,30 @@
 //Load HTTP module
 const http = require("http");
-const hostname = "127.0.0.1";
+const hostname = "192.168.100.25";
 const port = 3000;
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
   //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+
+  if (req.method == "POST") {
+    console.log("POST");
+    var body = "";
+    req.on("data", function (data) {
+      body += data;
+      console.log("Partial body: " + body);
+    });
+    req.on("end", function () {
+      console.log("image received");
+      res.writeHead(200, { "Content-Type": "image/jpeg" });
+      res.end("post received");
+    });
+  } else {
+    console.log("GET");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
+  }
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
